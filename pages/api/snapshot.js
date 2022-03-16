bffdfba8f41b4632c13a459a0d78e0ec31f5c767
Cameron Robertson <charles.c.robertson@gmail.com>
2022-03-16 15:07:31 -0600

    iterate through pagination.

--- pages/api/snapshot.js
+++ pages/api/snapshot.js
@@ -2,11 +2,35 @@ import {fetchSignatures, compareSigs} from "../../arweaveFns";
 
 const CANONICAL = process.env.CANONICAL || "1HhgIhjmE724U6SHVVhtIFLxGlCLy_h0O4SkA3IDhPY";
 
+let cursor = null;
+let sigs = [];
+
+// Iterate through results, append signatures, stop when we get to the end.
+async function fetchUntilEnd() {
+    let newCursor = cursor
+    let newSigs = await fetchSignatures(CANONICAL, cursor)
+    
+    if (newSigs.length > 0) {
+      sigs.push(...newSigs)
+      newCursor = newSigs.pop()["CURSOR"];
+    }
+
+    if (newCursor != cursor) {
+      cursor = newCursor;
+      await fetchUntilEnd()
+    } else {
+      return
+    }
+
+}
+
 // See https://github.com/snapshot-labs/snapshot-strategies/blob/master/src/strategies/api-post/examples.json for expected JSON format
 export default async (req, res) => {
     if (req.method === 'POST') {
-        const maybeSigs = await fetchSignatures(CANONICAL, null, 500)
-        const returnAddrs = compareSigs(req.body.addresses, maybeSigs)
+
+        await fetchUntilEnd()
+        const returnAddrs = compareSigs(req.body.addresses, sigs)
+        
         res.status(200).json({ score: returnAddrs })
       } else {
         res.status(400).json({ error: 'expecting POST' })
